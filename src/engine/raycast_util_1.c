2d2a6951ebaefabc84dea7c1688e41b0df9c72b7
yahya <yben0600@gmail.com>
2023-06-16 14:00:29 +0100

    cul

--- src/engine/raycast_util_1.c
+++ src/engine/raycast_util_1.c
@@ -6,7 +6,7 @@
 /*   By: ybenlafk <ybenlafk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 16:51:33 by ybenlafk          #+#    #+#             */
-/*   Updated: 2023/06/16 11:58:28 by ybenlafk         ###   ########.fr       */
+/*   Updated: 2023/06/16 13:51:28 by ybenlafk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,15 @@ void	horz_caluls(t_engine *p, t_data *data, float ray_angle)
 		p->y_intercept_h += TILE_SIZE;
 	p->x_intercept_h = get_intercept(data, p->y_intercept_h, ray_angle, 0);
 	p->y_step_h = TILE_SIZE;
-	if (p->is_ray_facing_up)
-		p->y_step_h *= -1;
 	p->x_step_h = TILE_SIZE / tan(ray_angle);
-	if (p->is_ray_facing_left && p->x_step_h > 0)
-		p->x_step_h *= -1;
-	if (p->is_ray_facing_right && p->x_step_h < 0)
-		p->x_step_h *= -1;
 	p->hit_h_x = p->x_intercept_h;
 	p->hit_h_y = p->y_intercept_h;
 	if (p->is_ray_facing_up)
+	{
+		p->y_step_h *= -1;
+		p->x_step_h *= -1;
 		p->hit_h_y -= 0.01;
+	}
 }
 
 void	get_horz(t_engine *p, t_data *data, float ray_angle)
@@ -70,17 +68,15 @@ void	vert_claculs(t_engine *p, t_data *data, float ray_angle)
 		p->x_intercept_v += TILE_SIZE;
 	p->y_intercept_v = get_intercept(data, p->x_intercept_v, ray_angle, 1);
 	p->x_step_v = TILE_SIZE;
-	if (p->is_ray_facing_left)
-		p->x_step_v *= -1;
 	p->y_step_v = TILE_SIZE * tan(ray_angle);
-	if (p->is_ray_facing_up && p->y_step_v > 0)
-		p->y_step_v *= -1;
-	if (p->is_ray_facing_down && p->y_step_v < 0)
-		p->y_step_v *= -1;
 	p->hit_v_x = p->x_intercept_v;
 	p->hit_v_y = p->y_intercept_v;
 	if (p->is_ray_facing_left)
+	{
+		p->y_step_v *= -1;
+		p->x_step_v *= -1;
 		p->hit_v_x -= 0.01;
+	}
 }
 
 void	get_vert(t_engine *p, t_data *data, float ray_angle)
